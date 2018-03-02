2596c84f01d84f91b42ee951d1bd91fc024247dd
Unknown <domagalski.dominik@gmail.com>
2018-03-02 21:01:55 +0100

    added full ntfsfix support

--- src/ntfsfixgui/Controller.java
+++ src/ntfsfixgui/Controller.java
@@ -3,6 +3,7 @@ package ntfsfixgui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import sun.rmi.runtime.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +15,8 @@ public class Controller {
     public Button refreshButton;
     public Button repairButton;
     public TextArea logTextArea;
+    public CheckBox clearBadSectorsCheckBox;
+    public CheckBox clearVolumeDirtyFlagCheckBox;
 
     @FXML
     public void initialize() throws IOException {
@@ -31,21 +34,30 @@ public class Controller {
     @FXML
     private void onRepairButtonClick(ActionEvent actionEvent) throws  IOException{
 
+
         logTextArea.clear();
         String toLogTextArea = new String();
+        String params = new String();
         String diskPath = disksComboBox.getSelectionModel().getSelectedItem().toString();
 
-        repairButton.setDisable( true );
-        refreshButton.setDisable( true );
+        //repairButton.setDisable( true );
+        //refreshButton.setDisable( true );
 
-        Process ntfsfix = Runtime.getRuntime().exec( "ntfsfix " + diskPath);
-        logTextArea.setText("Repairing...");
+        if( clearBadSectorsCheckBox.isSelected() ) params += "-b ";
+        if( clearVolumeDirtyFlagCheckBox.isSelected() ) params += "-d ";
+
+        String command = "ntfsfix " + params + diskPath;
+
+        Process ntfsfix = Runtime.getRuntime().exec( command );
+
+        logTextArea.setText("Running " + command + "\n");
 
         InputStream out = ntfsfix.getInputStream();
 
+        toLogTextArea = logTextArea.getText();
         int c;
         while ((c = out.read()) != -1) {
-            toLogTextArea = toLogTextArea + (char)c;
+            toLogTextArea += (char)c;
             logTextArea.setText(toLogTextArea);
         }
         out.close();
