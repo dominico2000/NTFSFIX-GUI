f8d4864d462dda0935cf5f624f78ffa8456d1442
Unknown <domagalski.dominik@gmail.com>
2018-03-02 19:52:48 +0100

    repair button onClick action

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Controller {
 
@@ -26,7 +27,32 @@ public class Controller {
 
         SupportClass.refreshDisksComboBox( disksComboBox, refreshButton);
     }
-    
+
+    @FXML
+    private void onRepairButtonClick(ActionEvent actionEvent) throws  IOException{
+
+        logTextArea.clear();
+        String toLogTextArea = new String();
+        String diskPath = disksComboBox.getSelectionModel().getSelectedItem().toString();
+
+        repairButton.setDisable( true );
+        refreshButton.setDisable( true );
+
+        Process ntfsfix = Runtime.getRuntime().exec( "ntfsfix " + diskPath);
+        logTextArea.setText("Repairing...");
+
+        InputStream out = ntfsfix.getInputStream();
+
+        int c;
+        while ((c = out.read()) != -1) {
+            toLogTextArea = toLogTextArea + (char)c;
+            logTextArea.setText(toLogTextArea);
+        }
+        out.close();
+
+        repairButton.setDisable( false );
+        refreshButton.setDisable( false );
+    }
 
 }
 
