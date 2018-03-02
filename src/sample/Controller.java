8d72e439c0a478b3550809a1765b12124b46ac3d
Unknown <domagalski.dominik@gmail.com>
2018-03-02 19:50:19 +0100

    Added Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+
 public class Controller {
+
+    @FXML
+    public ComboBox disksComboBox;
+    public Button refreshButton;
+    public Button repairButton;
+    public TextArea logTextArea;
+
+    @FXML
+    public void initialize() throws IOException {
+
+        SupportClass.refreshDisksComboBox( disksComboBox, refreshButton );
+
+    }
+
 }
+
